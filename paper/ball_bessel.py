6e25f4c33b0a3d2107fbaf83d2ec115ee4bcc6a4
Daniel Lecoanet <dlecoanet@gmail.com>
2020-02-12 15:19:58 +1100

    Bessel test works with new operators.

--- paper/ball_bessel.py
+++ paper/ball_bessel.py
@@ -1,27 +1,26 @@
-from dedalus_sphere import ball128 as ball
+from dedalus_sphere import ball
 import numpy as np
 from   scipy.linalg      import eig, inv
 import scipy.special     as spec
 
 # The eigenvalues should be the zeros of the half-integer-order Bessel functions; aka spherical bessel functions.
 
-def eigensystem(N,l,cutoff=1e9,report_error=False,a=0.0,method='sparse',worland=False):
+def eigensystem(N,l,cutoff=1e9,report_error=False,alpha=0.0,method='sparse',worland=False):
 
-    z,w = ball.quadrature(N,niter=3,report_error=report_error,a=a)
+    z,w = ball.quadrature(3, N, alpha=alpha, niter=3,report_error=report_error)
 
-    Q = ball.polynomial(N,0,l,z,a=a)
-
-    E0 = ball.operator('E',N,0,l,a=a)
-    E1 = ball.operator('E',N,1,l,a=a)
-    D0 = ball.operator('D+',N,0,l,a=a)
-    D1 = ball.operator('D-',N,1,l+1,a=a)
+    E0 = ball.operator(3, 'E',  N, 0, l, 0, alpha=alpha)
+    E1 = ball.operator(3, 'E',  N, 1, l, 0, alpha=alpha)
+    D0 = ball.operator(3, 'D+', N, 0, l, 0, alpha=alpha)
+    D1 = ball.operator(3, 'D-', N, 1, l, 1, alpha=alpha)
 
     R = E1.dot(E0)
     L = D1.dot(D0)
 
     L,R = L.todense(), R.todense()
-    L[N]=ball.operator('r=1',N,0,l,a=a)
-    R[N]=np.zeros(N+1)
+
+    L[-1]=ball.operator(3, 'r=R', N, 0, l, 0, alpha=alpha)
+    R[-1]*=0
 
     vals, vecs = eig(L,b=-R)
     bad = (np.abs(vals) > cutoff)
