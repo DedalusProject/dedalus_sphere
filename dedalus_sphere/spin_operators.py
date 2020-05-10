aa35321f052b21be62743cb1d726395b0de315a5
Geoff Vasil <flippy@192-168-1-101.tpgi.com.au>
2020-05-11 00:37:55 +1000

    tweak

--- dedalus_sphere/spin_operators.py
+++ dedalus_sphere/spin_operators.py
@@ -26,7 +26,93 @@ def tuple_array(elements,ranks,indexing):
             T[i,j] = elements[sigma,tau]
     return T
 
-class SpinCodomain():
+
+class Identity(Operator):
+    
+    def __init__(self,indexing=indexing):
+        
+        def identity(rank):
+            return tuple_array(self,(rank,rank),indexing)
+        
+        Operator.__init__(self,identity,TensorCodomain(0))
+        
+    @int2tuple
+    def __getitem__(self,i):
+        return i[0] == i[1]
+
+
+class Metric(Operator):
+    
+    def __init__(self,indexing=indexing):
+    
+        def metric(rank):
+            return tuple_array(self,(rank,rank),indexing)
+        
+        Operator.__init__(self,metric,TensorCodomain(0))
+    
+    @int2tuple
+    def __getitem__(self,i):
+        return i[0] == dual(i[1])
+    
+    
+class Transpose(Operator):
+    
+    def __init__(self,permutation=(1,0),indexing=indexing):
+    
+        def transpose(rank):
+            return tuple_array(self,(rank,rank),indexing)
+        
+        Operator.__init__(self,transpose,TensorCodomain(0))
+        self.__permutation = permutation
+    
+    @property
+    def permutation(self):
+        return self.__permutation
+    
+    @int2tuple
+    def __getitem__(self,i):
+        return i[0] == apply(self.permutation)(i[1])
+
+class Trace(Operator):
+    
+    def __init__(self,indices=(0,1),indexing=indexing):
+    
+        def trace(rank):
+            return tuple_array(self,(rank-len(indices),rank),indexing)
+        
+        Operator.__init__(self,trace,TensorCodomain(-len(indices)))
+        
+        self.__indices  = indices
+    
+    @property
+    def indices(self):
+        return self.__indices
+    
+    @int2tuple
+    def __getitem__(self,i):
+        return i[0] == remove(self.indices)(i[1]) and sum_(self.indices)(i[1]) == 0
+    
+class TensorProduct(Operator):
+    
+    def __init__(self,element,indexing=indexing):
+        if type(element) == int: element = (element,)
+        
+        def product(rank):
+            return tuple_array(self,(rank+len(element),rank),indexing)
+        
+        Operator.__init__(self,product,TensorCodomain(len(element)))
+        self.__element = element
+    
+    @property
+    def element(self):
+        return self.__element
+
+    @int2tuple
+    def __getitem__(self,i):
+        return i[0] == self.element + i[1]
+    
+
+class TensorCodomain():
 
     def __init__(self,rank_change):
         self.__arrow = rank_change
@@ -34,17 +120,17 @@ class SpinCodomain():
     @property
     def arrow(self):
         return self.__arrow
-    
+
     def __str__(self):
         s = f'(rank->rank+{self.arrow})'
         return s.replace('+0','').replace('+-','-')
-    
+
     def __repr__(self):
         return str(self)
         
     def __add__(self,other):
-        return SpinCodomain(self.arrow+other.arrow)
-    
+        return TensorCodomain(self.arrow+other.arrow)
+
     def __call__(self,other):
         if self.arrow + other < 0:
             raise ValueError('cannot map to negative rank.')
@@ -64,97 +150,10 @@ class SpinCodomain():
     def __mul__(self,other):
         if type(other) != int:
             raise TypeError('only integer multiplication defined.')
-        return SpinCodomain(other*self.arrow)
-    
+        return TensorCodomain(other*self.arrow)
+
     def __rmul__(self,other):
         return self*other
-    
+
     def __sub__(self,other):
         return self + (-other)
-
-
-class Identity(Operator):
-    
-    def __init__(self,indexing=indexing):
-        
-        def identity(rank):
-            return tuple_array(self,(rank,rank),indexing)
-        
-        Operator.__init__(self,identity,SpinCodomain(0))
-        
-    @int2tuple
-    def __getitem__(self,i):
-        return i[0] == i[1]
-
-
-class Metric(Operator):
-    
-    def __init__(self,indexing=indexing):
-    
-        def metric(rank):
-            return tuple_array(self,(rank,rank),indexing)
-        
-        Operator.__init__(self,metric,SpinCodomain(0))
-    
-    @int2tuple
-    def __getitem__(self,i):
-        return i[0] == dual(i[1])
-    
-    
-class Transpose(Operator):
-    
-    def __init__(self,permutation=(1,0),indexing=indexing):
-    
-        def transpose(rank):
-            return tuple_array(self,(rank,rank),indexing)
-        
-        Operator.__init__(self,transpose,SpinCodomain(0))
-        self.__permutation = permutation
-    
-    @property
-    def permutation(self):
-        return self.__permutation
-    
-    @int2tuple
-    def __getitem__(self,i):
-        return i[0] == apply(self.permutation)(i[1])
-
-class Trace(Operator):
-    
-    def __init__(self,indices=(0,1),indexing=indexing):
-    
-        def trace(rank):
-            return tuple_array(self,(rank-len(indices),rank),indexing)
-        
-        Operator.__init__(self,trace,SpinCodomain(-len(indices)))
-        
-        self.__indices  = indices
-    
-    @property
-    def indices(self):
-        return self.__indices
-    
-    @int2tuple
-    def __getitem__(self,i):
-        return i[0] == remove(self.indices)(i[1]) and sum_(self.indices)(i[1]) == 0
-    
-class TensorProduct(Operator):
-    
-    def __init__(self,element,indexing=(-1,0,1)):
-        if type(element) == int: element = (element,)
-        
-        def product(rank):
-            return tuple_array(self,(rank+len(element),rank),indexing)
-        
-        Operator.__init__(self,product,SpinCodomain(len(element)))
-        self.__element = element
-    
-    @property
-    def element(self):
-        return self.__element
-
-    @int2tuple
-    def __getitem__(self,i):
-        return i[0] == self.element + i[1]
-    
-
